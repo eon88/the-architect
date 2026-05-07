4cca1398e311f70b496bce0c5d69dbbf4a933e64
eon88 <eon88tfn@gmail.com>
2026-05-07 23:33:55 +0100

    Fix: Use absolute path for template directory to resolve page loading issue
    - Import `os` module
    - Change Jinja2Templates directory from relative path 'templates' to absolute path using os.path.dirname(__file__)
    - This ensures templates are found regardless of where the app is started from

--- web/main.py
+++ web/main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import random
+import os
 
 app = FastAPI()
-templates = Jinja2Templates(directory='templates')
+templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), 'templates'))
 
 # Mock Data
 PILLARS = {
